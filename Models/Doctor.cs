acb78ab86a1fb9bd88c40159436cdf5dea229a88
Veselin <veselin.andonov@gmail.com>
2026-03-23 23:44:55 +0200

    2

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ClinicSystem_22180011.Models;
 
@@ -9,9 +10,11 @@ public partial class Doctor
 
     public string FullName { get; set; } = null!;
 
+    [Display(Name = "Специалност")]
     public string? Specialty { get; set; }
 
-    public DateTime? LastModified22180011 { get; set; }
+    [ConcurrencyCheck]
+    public DateTime? LastModified_22180011 { get; set; }
 
     public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
 }
